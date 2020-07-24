a8e6c737f3b271520255f0867186c2a0fe6ac4ff
unknown <ogbemudiatimothy@gmail.com>
2020-07-24 03:38:53 +0100

    initial setup in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
