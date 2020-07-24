f0ad846d0ebe19fecc545717b0260e7f45ff892e
unknown <ogbemudiatimothy@gmail.com>
2020-07-24 03:43:45 +0100

    added dotenv and baseurl value

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import { config } from 'dotenv';
 const app = express();
+config();
 app.use(morgan('dev'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
